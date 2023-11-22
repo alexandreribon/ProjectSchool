d8223b3bb684db8f69b3de7d08bceda82035d7e9
Alexandre Ribon <xribon@gmail.com>
2023-11-22 10:23:14 -0300

    Criando Layout da Lista de Cursos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,7 +14,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FlexLayoutModule
   ],
   exports: [
-    ToolbarTitleComponent
+    ToolbarTitleComponent,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
