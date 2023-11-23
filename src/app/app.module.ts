2e0f64a39714692d1c85853721183b73a70a84bc
Alexandre Ribon <xribon@gmail.com>
2023-11-23 09:09:29 -0300

    Carregando os dados de cursos na tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/materials/material.module';
 import { ToolbarMenuComponent } from './shared/components/toolbar-menu/toolbar-menu.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ToolbarMenuComponent } from './shared/components/toolbar-menu/toolbar-m
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/courses/course-list/course-list.component.ts
+++ src/app/pages/courses/course-list/course-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '@app/services/courses.service';
+import { Course } from '@app/shared/models/course';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
-  constructor() {}
+  public courseList: Course[] = [];
+
+  constructor(private courseService: CoursesService) {}
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  public getCourses(): void {
+    this.courseService.getAll().subscribe({
+      next: (response: Course[]) => {
+        this.courseList = response;
+      }
+    });
+  }
 }
